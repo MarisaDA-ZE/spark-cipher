d1402fcc5b6858ef19a5a43636b48580f8e2eab0
jiangxingwu@pxaionline.com <3038488053@qq.com>
2023-11-27 15:53:39 +0800

    初始化项目

---
+++ src/main/java/top/kirisamemarisa/sparkcipher/SparkCipherApplication.java
@@ -0,0 +1,13 @@
+package top.kirisamemarisa.sparkcipher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkCipherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkCipherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/kirisamemarisa/sparkcipher/SparkCipherApplicationTests.java
@@ -0,0 +1,13 @@
+package top.kirisamemarisa.sparkcipher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkCipherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
